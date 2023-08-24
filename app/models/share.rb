6dc5fbc9e28eb8e4d486085606b5871f5a887683
HaNguyen32 <juhi_nguyen.hfl@outlook.jp>
2023-08-24 21:18:42 +0900

    validatesを再設定

--- app/models/share.rb
+++ app/models/share.rb
@@ -2,17 +2,8 @@ class Share < ApplicationRecord
     has_one_attached :image
     belongs_to :user
     with_options presence: true do
-        validates :image
+        #validates :image
         validates :title 
-        validates :area
-        validates :mountain_area
-        validates :mountain_name 
-        validates :date_entry
-        validates :date_exit
-        validates :access
-        validates :course
-        validates :itinerary
-        validates :activity_record
         validates :user_id
     end
 end
