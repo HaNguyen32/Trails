14f5be75e4e7db7edda9388c2115212cf51cce19
HaNguyen32 <juhi_nguyen.hfl@outlook.jp>
2023-08-25 11:53:10 +0900

    バリデーション定義

--- app/models/share.rb
+++ app/models/share.rb
@@ -2,7 +2,7 @@ class Share < ApplicationRecord
     has_one_attached :image
     belongs_to :user
     with_options presence: true do
-        #validates :image
+        validates :image
         validates :title 
         validates :user_id
     end
